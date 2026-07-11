4ba2f7d275f5a1e04011c14dd6da68705a061a92
djh1984investing-eng <djh1984investion@gmail.com>
2026-07-10 23:07:13 -0400

    Serve sitemap and robots as static files

--- api/robots.js
+++
@@ -1,9 +0,0 @@
-export default function handler(_req, res) {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.setHeader("Cache-Control", "public, max-age=3600");
-  res.status(200).send(`User-agent: *
-Allow: /
-
-Sitemap: https://www.futuresplaymaker.com/sitemap.xml
-`);
-}
